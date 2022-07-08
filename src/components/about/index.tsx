e4d0a825c5075f8f7b88e57a695317fac54c5426
ValentinLagues <valentin.lagues@gmail.com>
2022-07-09 01:24:43 +0200

    skills done

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -1,16 +1,16 @@
-import SectionContainer from "../sectionContainer";
-import AboutText from "./AboutText";
+import SectionContainer from "@components/sectionContainer";
 import AboutTitle from "./AboutTitle";
+import AboutText from "./AboutText";
 
 const AboutComponent = (): JSX.Element => {
-  return (
-    <SectionContainer sectionId={"about"}>
-      <div className="w-full h-full min-h-screen pt-20 md:pt-10">
-        <AboutTitle />
-        <AboutText />
-      </div>
-    </SectionContainer>
-  );
+    return (
+        <SectionContainer sectionId={"about"}>
+            <div className="w-full h-full min-h-screen pt-20 md:pt-10">
+                <AboutTitle />
+                <AboutText />
+            </div>
+        </SectionContainer>
+    );
 };
 
 export default AboutComponent;
---
+++ src/components/skills/SkillImage.tsx
@@ -0,0 +1,54 @@
+import Image, { StaticImageData } from "next/image";
+import { useEffect, useState } from "react";
+// import useInteraction from "../../hooks/useInteraction";
+
+interface IPlayer {
+  id: number;
+  pic: string | StaticImageData;
+  alt: string;
+}
+
+interface IProps {
+  pic: IPlayer;
+  modalIsOpen: number;
+  setModalIsOpen: React.Dispatch<React.SetStateAction<number>>;
+}
+
+export default function SkillImage({
+  pic,
+  modalIsOpen,
+  setModalIsOpen,
+}: IProps) {
+  const [picSelected, setPicSelected] = useState("");
+  // const { option, select } = useInteraction();
+
+  useEffect(() => {
+    if (modalIsOpen === pic.id) {
+      setPicSelected("border-x-2 border-y-4 border-yellow-600");
+    } else {
+      setPicSelected("border-x-2 border-y-4 border-white");
+    }
+  }, [modalIsOpen, pic.id]);
+
+  const handleClick = () => {
+    if (modalIsOpen !== pic.id) {
+      setModalIsOpen(pic.id);
+      // select();
+    } else {
+      setModalIsOpen(0);
+    }
+  };
+
+  return (
+    <div className={`${picSelected} relative cursor-pointer h-48 w-48`}>
+      <Image
+        objectFit="cover"
+        layout="fill"
+        src={pic.pic}
+        onClick={() => handleClick()}
+        priority={true}
+        alt={pic.alt}
+      />
+    </div>
+  );
+}
