563132444a50ee0720be4be396c7198b3a8c12a3
ValentinLagues <valentin.lagues@gmail.com>
2022-07-10 01:09:43 +0200

    roadrage added

--- src/components/home/HomeTitle.tsx
+++ src/components/home/HomeTitle.tsx
@@ -4,7 +4,7 @@ const HomeTitle = (): JSX.Element => {
   const { t } = useTranslation();
   return (
     <div className="w-full text-white ">
-      <h1 className="flex flex-row justify-center text-4xl font-roadrage navbartitle md:text-7xl lg:text-8xl xl:text-8xl">
+      <h1 className="flex flex-row justify-center text-4xl font-roadrage mainTitle md:text-7xl lg:text-8xl xl:text-9xl">
         {t("home.name")}
       </h1>
       <h2 className="flex text-3xl md:py-4 title md:text-6xl lg:text-7xl xl:text-8xl">
