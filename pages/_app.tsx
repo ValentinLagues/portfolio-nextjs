b28ea2e0de392ffd75bd53278ab6e63bf5847223
ValentinLagues <valentin.lagues@gmail.com>
2022-07-09 03:42:13 +0200

    seo added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,14 +5,37 @@ import "@styles/global.css";
 import "../i18n";
 import Layout from "@components/layout";
 import { AppContextProvider } from "src/context/Context";
+import { DefaultSeo } from "next-seo";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <AppContextProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </AppContextProvider>
+    <>
+      <DefaultSeo
+        title="Valentin Lagues : Web developer"
+        description="Javascript full-stack developer"
+        canonical="https://portfolio-valentinlagues.vercel.app/"
+        openGraph={{
+          url: "https://portfolio-valentinlagues.vercel.app/",
+          title: "Valentin Lagues : Web developer",
+          description:
+            "Javascript full-stack developer, front end, back end, React",
+          images: [
+            {
+              url: "https://res.cloudinary.com/dnw0kiu56/image/upload/v1657185046/portfolio/seologo_mcazhi.jpg",
+              width: 200,
+              height: 200,
+              alt: "logo",
+            },
+          ],
+          site_name: "Valentin Lagues : Web developer",
+        }}
+      />
+      <AppContextProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </AppContextProvider>
+    </>
   );
 }
 
