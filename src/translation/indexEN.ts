c14c661dcfeea83615c1c08e23012d2cc8b5cf0e
ValentinLagues <valentin.lagues@gmail.com>
2022-08-07 16:51:17 +0200

    ihoover added

--- src/translation/indexEN.ts
+++ src/translation/indexEN.ts
@@ -1,13 +1,13 @@
-import headerTrad from '../../content/header.json';
-import footerTrad from '../../content/footer.json';
-import homeTrad from '../../content/home.json';
-import optionsTrad from '../../content/options.json';
-import aboutTrad from '../../content/about.json';
-import skillsTrad from '../../content/skills.json';
+import headerTrad from "../../content/header.json";
+import footerTrad from "../../content/footer.json";
+import homeTrad from "../../content/home.json";
+import optionsTrad from "../../content/options.json";
+import aboutTrad from "../../content/about.json";
+import skillsTrad from "../../content/skills.json";
 import skillsListTrad from "../../content/skillsList.json";
-import projectsTrad from '../../content/projects.json';
-import projectsListTrad from '../../content/projectsList.json';
-import contactTrad from '../../content/contact.json';
+import projectsTrad from "../../content/projects.json";
+import projectsListTrad from "../../content/projectsList.json";
+import contactTrad from "../../content/contact.json";
 
 const header = headerTrad.en;
 const footer = footerTrad.en;
@@ -20,18 +20,17 @@ const projects = projectsTrad.en;
 const projectsList = projectsListTrad.en;
 const contact = contactTrad.en;
 
-
 const ressourcesEn = {
-header,
-footer,
-home,
-options,
-about,
-skills,
-skillsList,
-projects,
-projectsList,
-contact
+  header,
+  footer,
+  home,
+  options,
+  about,
+  skills,
+  skillsList,
+  projects,
+  projectsList,
+  contact,
 };
 
 export default ressourcesEn;
